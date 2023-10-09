6feec607b2d40e7a5a0a32e626ee17dfe6033350
Ramasilo <preciousramasilo97@gmail.com>
2023-10-09 03:11:37 +0200

    Added the Github repo link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello world..</h1>
+      <footer>
+        This project was coded by Precious Ramasilo and is{" "}
+        <a
+          href="https://github.com/Ramasilo/my-weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
