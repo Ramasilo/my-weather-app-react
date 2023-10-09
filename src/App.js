53f4e404cce2decf409366e4f0e00caf1b594f1e
Ramasilo <preciousramasilo97@gmail.com>
2023-10-09 03:33:32 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello world..</h1>
-      <footer>
-        This project was coded by Precious Ramasilo and is{" "}
-        <a
-          href="https://github.com/Ramasilo/my-weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Hello world..</h1>
+        <footer>
+          This project was coded by Precious Ramasilo and is{" "}
+          <a
+            href="https://github.com/Ramasilo/my-weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
