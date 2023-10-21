86d05cb4424f2b087c9b2e693188f6b3a5771449
Ramasilo <preciousramasilo97@gmail.com>
2023-10-21 04:46:39 +0200

    Revert "implement daily forecast"
    This reverts commit 09e39c014cfee64bafeb2f60d70d4374b7e6eb83.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -18,15 +18,9 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          {forecast.map(function (dailyForecast, index) {
-            if (index < 6) {
-              return (
-                <div className="col" key={index}>
-                  <WeatherForecastDay data={dailyForecast} />
-                </div>
-              );
-            }
-          })}
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
         </div>
       </div>
     );
